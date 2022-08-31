21e8e6e6fd1c8b3ddd8ee2b59f1a3054d1ddd472
jacopo-marcolini-bid <jacopo.marcolini@bidcompany.it>
2022-08-31 17:42:58 +0200

    [add] loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,25 @@ export class AppComponent {
   ) { }
 
   elements: Array<any> = [];
+  loading: boolean = false;
+  loadingTimer: any;
 
   ngOnInit(): void {
     this.getPathContent();
   }
 
   getPathContent() {
+    /* show loading animation if user is waiting for > 100ms */
+    this.loadingTimer = setTimeout(() => {
+      this.loading = true;
+      this.elements = [];
+    }, 100)
+    
     this.explorerService.getPathContent()
       .subscribe((data) => {
         this.elements = data;
+        clearInterval(this.loadingTimer);
+        this.loading = false;
       });
   }
 }
